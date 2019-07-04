02920cc0d7fb31974f5e470d2b086b46533efa89
DariuszDyrda <darek.d.az.nie@interia.pl>
2019-07-04 19:48:02 +0200

    add entities to TypeOrmModule imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature()],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- src/weight/weight.module.ts
+++ src/weight/weight.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WeightService } from './weight.service';
 import { WeightController } from './weight.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Weight } from './weight.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature()],
+    imports: [TypeOrmModule.forFeature([Weight])],
     providers: [WeightService],
     controllers: [WeightController],
 })
